ccb423fa82fcdd96760f9ee8d71950244a4ff9ad
andycloke <apcloke@gmail.com>
2017-01-05 12:30:52 +0000

    tested and fixed resize

--- pset4/resize/resize.c
+++ pset4/resize/resize.c
@@ -1,144 +1,89 @@
 /**
- * Resizes a BMP.
- */
+ * recovers JPEGs from a file
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "bmp.h"
-
 int main(int argc, char *argv[])
 {
-    // ensure proper usage
-    if (argc != 4)
+    // ensure correct image
+    if (argc != 2)
     {
-        fprintf(stderr, "Usage: ./resize n infile outfile\n");
-        return 1;
-    }
-
-    // resize factor
-    int n = atoi(argv[1]);
-
-    if (n <= 0 || n > 100)
-    {
-        fprintf(stderr, "n must be a positive integer less than 100\n");
+        fprintf(stderr, "Usage ./recover forensicimage\n");
         return 1;
     }
 
     // remember filenames
-    char *infile = argv[2];
-    char *outfile = argv[3];
+    char *infile = argv[1];
 
-    // open input file
+    // open input file (memory card)
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
-        fprintf(stderr, "Could not open %s.\n", infile);
+        fprintf(stderr, "Could not open %s\n", infile);
         return 1;
     }
 
-    // open output file
-    FILE *outptr = fopen(outfile, "w");
-    if (outptr == NULL)
+    // bSize
+    int bSize = 512;
+
+    // counter for jpg filenames
+    int counter = 0;
+
+    // storage for buffer
+    unsigned char* buffer = malloc(bSize);
+
+
+    // output pointer;
+    FILE *img = NULL;
+
+    // repeat until end of card (when number of bytes returned will < bSize)
+    while (fread(buffer, 1, bSize, inptr) == bSize)
     {
-        fclose(inptr);
-        fprintf(stderr, "Could not create %s.\n", outfile);
-        return 1;
-    }
 
-    // read infile's BITMAPFILEHEADER
-    BITMAPFILEHEADER bf;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
-
-    // read infile's BITMAPINFOHEADER
-    BITMAPINFOHEADER bi;
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
-
-    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
-        bi.biBitCount != 24 || bi.biCompression != 0)
-    {
-        fclose(outptr);
-        fclose(inptr);
-        fprintf(stderr, "Unsupported file format.\n");
-        return 4;
-    }
-
-    //adjust headers
-
-    // remember original width and height
-    int widthIn = bi.biWidth;
-    int heightIn = bi.biHeight;
-
-    //resize width and height;
-    bi.biWidth *= n;
-    bi.biHeight *= n;
-
-    //calculate original padding
-    int paddingIn = (4 - (widthIn * sizeof(RGBTRIPLE)) % 4) % 4;
-
-    //calculate output padding
-    int paddingOut = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
-    //resize biSizeImage
-    bi.biSizeImage = (bi.biWidth + paddingOut) * sizeof(RGBTRIPLE) * abs(bi.biHeight);
-
-    //resize bfSize
-    bf.bfSize = bi.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
-    // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(heightIn); i < biHeight; i++)
-    {
-        // array to store pixels in
-        RGBTRIPLE pixels[widthIn];
-
-        // iterate over pixels in scanline and store values
-        for (int j = 0; j < widthIn; j++)
+        // start of new JPEG?
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // temporary storage
-            RGBTRIPLE triple;
-
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
-            // store value in array
-            pixels[j] = triple;
-        }
-
-        // repeat vertically
-        for (int k = 0; k < n; k++)
-        {
-            // for each value in array,
-            for (int j = 0; j < widthIn; j++)
+            // if jpegs already found, close them
+            if (counter)
             {
-                //  print n times to resize horizontally
-                for (int p = 0; p < n; p++)
-                {
-                    fwrite(&pixels[j], sizeof(RGBTRIPLE), 1, outptr);
-                }
-                // add output padding
-                for (int k = 0; k < paddingOut; k++)
-                {
-                    fputc(0x00, outptr);
-                }
+                fclose(img);
             }
+
+            char *filename = NULL;
+
+            // create name for new file
+            sprintf(filename, "%03i.jpg", counter);
+
+            // create output file
+            img = fopen(filename, "w");
+            if (img == NULL)
+            {
+                fprintf(stderr, "Could not create %s.jpg \n", filename);
+                return 1;
+            }
+
+            // increment counter
+            counter++;
         }
-        // skip over input padding, if any
-        fseek(inptr, paddingIn, SEEK_CUR);
+
+        // if we have hit jpegs, write to output file
+        if (img != NULL)
+        {
+        fwrite(buffer, bSize, 1, img);
+        }
     }
 
-    // close infile
+    // free buffer
+    free(buffer);
+
+    // close input file
     fclose(inptr);
 
-    // close outfile
-    fclose(outptr);
-
-    // success
-    return 0;
+    // close output jpg if there is one
+    if (img != NULL)
+    {
+        fclose(img);
+    }
 }
