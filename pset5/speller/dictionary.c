07bba4cadf78acf694f1c413536355c922b8f83a
andycloke <apcloke@gmail.com>
2017-01-06 14:31:51 +0000

    implemented laod & check

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -2,16 +2,68 @@
  * Implements a dictionary's functionality.
  */
 
+#include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
+
+// hash table size = 2^16
+const int HASHTABLE_SIZE = 65536;
+
+// define linked list node
+typedef struct node
+{
+    char word[LENGTH+1];
+    struct node *next;
+} node;
+
+// initialise hash table
+node *hash_table[HASHTABLE_SIZE];
+
+/**
+* Returns integer hash value for a given a string
+* Credit: Brenda's Husband: https://www.reddit.com/r/cs50/comments/1x6vc8/pset6_trie_vs_hashtable/cf9nlkn/
+**/
+int hash(const char* needs_hashing)
+{
+    unsigned int hash = 0;
+    for (int i=0, n=strlen(needs_hashing); i<n; i++)
+        hash = (hash << 2) ^ needs_hashing[i];
+    return hash % HASHTABLE_SIZE;
+}
 
 /**
  * Returns true if word is in dictionary else false.
  */
 bool check(const char *word)
 {
-    // TODO
+
+    // initialise lower case word
+    char lcword[LENGTH+1];
+
+    // convert to lowercase, as we need this to lookup. TODO: change from length to length of word
+    for (int i = 0; i < LENGTH; i++)
+    {
+        lcword[i] = tolower(word[i]);
+    }
+
+    // set cursor to start of appropriate lined list
+    node *cursor = hash_table[hash(lcword)];
+
+
+    // traverse list
+    while (cursor != NULL)
+    {
+        // check node's word to see if it is target word, ignoring case
+        if (strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
@@ -20,8 +72,41 @@ bool check(const char *word)
  */
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // initialise word
+    char word[LENGTH+1];
+
+    // open dictionary
+    FILE *dicptr = fopen(dictionary, "r");
+
+    // iterate through dictionary words
+    while (fscanf(dicptr, "%s", word) != EOF)
+    {
+        // make a new word. TODO: free
+        node *new_node = malloc(sizeof(node));
+
+        // check for error assigning memory
+        if (new_node == NULL)
+        {
+            unload();
+            return false;
+        }
+        else
+        {
+            // copy word into node
+            strcpy(new_node->word, word);
+
+            // use hash function to determine which bucket (linked list) to insert word into
+            int n = hash(new_node->word);
+
+            // insert into list
+            new_node->next = hash_table[n];
+            hash_table[n] = new_node;
+        }
+    }
+    // close dictionary
+    fclose(dicptr);
+
+    return true;
 }
 
 /**
